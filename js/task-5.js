de1c91e3356409059568b44480311695b123463c
RooK| <anton.smishko2014@gmail.com>
2024-08-25 21:48:50 +0300

    task-5
    without styles

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+   return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector(' .color');
+const changeColor = event => {
+   document.body.style.backgroundColor = getRandomHexColor();
+   spanColor.textContent = getRandomHexColor();
+};
+btnChangeColor.addEventListener('click', changeColor);
