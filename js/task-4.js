acda58fb308c5fb61e02a9e8f8bf756ea7c8b48c
RooK| <anton.smishko2014@gmail.com>
2024-08-26 11:19:51 +0300

    task-4
    add preventDef

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,8 @@
+const loginFormLink = document.querySelector('.login-form');
 
+const SubmitHandler = event => {
+   console.log(event);
+   event.preventDefault();
+};
+
+loginFormLink.addEventListener('submit', SubmitHandler);
