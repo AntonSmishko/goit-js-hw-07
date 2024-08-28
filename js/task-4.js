8171635cbf2124526146dc83690892c5be4af8a8
RooK| <anton.smishko2014@gmail.com>
2024-08-28 18:27:30 +0300

    task-4 without styles

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,22 @@
 const loginFormLink = document.querySelector('.login-form');
 
-const SubmitHandler = event => {
-   console.log(event);
+const submitHandler = event => {
    event.preventDefault();
+   const { email, password } = event.target.elements;
+   if (email.value === '' || password.value === '') {
+      alert('All form fields must be filled in');
+   }
+
+   //    console.dir(event.target.elements.email.value);
+   //    console.dir(event.target.elements.password.value);
+
+   let inputInfo = {
+      email: email.value,
+      password: password.value,
+   };
+   console.log(inputInfo);
+
+   event.target.reset();
 };
 
-loginFormLink.addEventListener('submit', SubmitHandler);
+loginFormLink.addEventListener('submit', submitHandler);
