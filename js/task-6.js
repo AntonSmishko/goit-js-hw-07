e440f7e1dfb8459e67a0fe52552d361ad1eb965c
RooK| <anton.smishko2014@gmail.com>
2024-08-28 22:05:19 +0300

    task - 6
    without css

--- js/task-6.js
+++ js/task-6.js
@@ -3,16 +3,29 @@ function getRandomHexColor() {
       .toString(16)
       .padStart(6, 0)}`;
 }
-
+const inputEl = document.querySelector('#controls > input');
 const boxesEl = document.querySelector('#boxes');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDesEl = document.querySelector('[data-destroy]');
 
-function createBoxes(amount) {
-   for (let i = 0; i <= amount; i += 1) {
-      const divEl = document.createElement('div');
-      divEl.style.width = 30;
-      divEl.style.height = 30;
-      divEl.style.backgroundColor = getRandomHexColor();
-   }
+btnCreateEl.addEventListener('click', () => createBoxes(Number(inputEl.value)));
+btnDesEl.addEventListener('click', destroyBoxes);
+
+function destroyBoxes(evt) {
+   boxesEl.innerHTML = '';
+   inputEl.value = '';
 }
 
-function destroyBoxes() {}
+function createBoxes(amount) {
+   boxesEl.innerHTML = '';
+
+   let divElSize = 30;
+   for (let i = 0; i < amount; i += 1) {
+      const divEl = document.createElement('div');
+      divEl.style.width = `${divElSize}px`;
+      divEl.style.height = `${divElSize}px`;
+      divEl.style.backgroundColor = getRandomHexColor();
+      boxesEl.append(divEl);
+      divElSize += 10;
+   }
+}
