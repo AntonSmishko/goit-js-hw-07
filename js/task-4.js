834c0046614b0cb6d164d623574dfa816b83125c
RooK| <anton.smishko2014@gmail.com>
2024-08-28 22:36:12 +0300

    update-task-4
    add else block

--- js/task-4.js
+++ js/task-4.js
@@ -5,16 +5,16 @@ const submitHandler = event => {
    const { email, password } = event.target.elements;
    if (email.value === '' || password.value === '') {
       alert('All form fields must be filled in');
+   } else {
+      let inputInfo = {
+         email: email.value,
+         password: password.value,
+      };
+      console.log(inputInfo);
    }
 
    //    console.dir(event.target.elements.email.value);
    //    console.dir(event.target.elements.password.value);
-
-   let inputInfo = {
-      email: email.value,
-      password: password.value,
-   };
-   console.log(inputInfo);
 
    event.target.reset();
 };
