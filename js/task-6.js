352d8b504e987e91ea04cd48a39f35aec269b7e0
RooK| <anton.smishko2014@gmail.com>
2024-08-28 20:04:37 +0300

    task - 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+   return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
 }
+
+const boxesEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+   for (let i = 0; i <= amount; i += 1) {
+      const divEl = document.createElement('div');
+      divEl.style.width = 30;
+      divEl.style.height = 30;
+      divEl.style.backgroundColor = getRandomHexColor();
+   }
+}
+
+function destroyBoxes() {}
